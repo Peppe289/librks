c44a9edc1519e0e2fdcb9784818ab919b43d460a
Peppe289 <gsperanza204@protonmail.com>
2023-11-19 00:28:18 +0100

    remove build file
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peppe289/Desktop/Kernel_Monitor/shared_lib
-# Build directory: /home/peppe289/Desktop/Kernel_Monitor/shared_lib/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
